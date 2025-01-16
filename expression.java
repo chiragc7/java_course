e1f5afc7b709291f323d5eb35fe93d631375489b
CHIRAG CHAUHAN <chiragchauhan10614@gmail.com>
2025-01-16 14:46:26 +0530

    Add files via upload

---
+++ expression.java
@@ -0,0 +1,8 @@
+public class expression {
+    public static void main(String[] args) {
+        System.out.println("value of expresstion is :");
+        float a =7/4.0f * 9/2.0f;
+        System.out.println(a);
+    }
+    
+}
