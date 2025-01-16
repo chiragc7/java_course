2794de1175f37c397deede9d51b58b6b108d3f3b
CHIRAG CHAUHAN <chiragchauhan10614@gmail.com>
2025-01-16 14:47:54 +0530

    Add files via upload

---
+++ grade_change.java
@@ -0,0 +1,10 @@
+public class grade_change {
+    public static void main(String[] args) {
+        char grade='B';
+        System.out.println("grade is :"+grade);
+        grade=(char)(grade+7);
+        System.out.println("change grade is :"+ grade);
+
+    }
+    
+}
---
+++ quick_quiz.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+    public class quick_quiz {
+        public static void main(String[] args) {
+        // write the foing expression
+        /* 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int x= sc.nextInt();
+        int y = sc.nextInt();
+        int z= (x-y)/2;
+        System.out.println(z);
+        */
+
+//another exprestion
+
+        /* 
+        int a=10;
+        int b=4;
+        int c=5;
+
+        int result=(b*b)-(4*a*c)/(2*a);
+        System.out.println(result);
+
+        */
+
+       // another exprestion
+
+     int v=2;
+     int u=2;
+     int result=v*v-u*u;
+     System.out.println(result);
+    }
+    
+}
